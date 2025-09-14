31be66dcd9610a0d6de69c6780d4e3ee756efe0a
the-eris-net <the.eris.net@gmail.com>
2025-09-15 01:07:17 +0900

    feat: init location

---
+++ 48p-location_href/src/About.jsx
@@ -0,0 +1,44 @@
+import { NavLink, Outlet, useOutletContext, Navigate } from 'react-router-dom';
+
+export function Song() {
+  return (
+    <div>
+      <h2>About A Girl</h2>
+      <p>너바나의 노래</p>
+    </div>
+  );
+}
+
+export function Movie() {
+  return (
+    <div>
+      <h2>About Time</h2>
+      <p>레이첼 맥 아담스 주연 영화</p>
+    </div>
+  );
+}
+
+export default function About() {
+  const { logined } = useOutletContext();
+
+  /*렌더링 도중에 시행해서 실제로 에러남, 그래서 이 방법 쓸려면 useEffect나 setTimeout */
+  if (!logined) {
+    setTimeout(() => {
+      history.pushState({}, '', '/');
+      dispatchEvent(new PopStateEvent('popstate'));
+    }, 0);
+  }
+
+  return (
+    <div>
+      <h2>About Page</h2>
+      <p>로그인 상태 : {logined ? '로그인' : '로그아웃'}</p>
+      <nav>
+        <NavLink to="song">노래</NavLink> | {' '}
+        <NavLink to="movie">영화</NavLink> | {' '}
+        <NavLink to="/">홈으로</NavLink>
+      </nav>
+      <Outlet />
+    </div>
+  );
+}
