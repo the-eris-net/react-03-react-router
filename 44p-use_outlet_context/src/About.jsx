90e8188ea62c2659a31ed2a08ab475010eb24843
the-eris-net <the.eris.net@gmail.com>
2025-09-15 00:21:28 +0900

    feat:finish use_outlet_context

--- 44p-use_outlet_context/src/About.jsx
+++ 44p-use_outlet_context/src/About.jsx
@@ -26,7 +26,8 @@ export default function About() {
       <h2>About Page</h2>
       <p>로그인 상태 : {logined ? '로그인' : '로그아웃'}</p>
       <nav>
-        <NavLink to="song">노래</NavLink> | <NavLink to="movie">영화</NavLink> |{' '}
+        <NavLink to="song">노래</NavLink> | {' '}
+        <NavLink to="movie">영화</NavLink> | {' '}
         <NavLink to="/">홈으로</NavLink>
       </nav>
       <Outlet />
--- 44p-use_outlet_context/src/App.jsx
+++ 44p-use_outlet_context/src/App.jsx
@@ -5,13 +5,18 @@ import './App.css';
 export default function App() {
   const [logined, setLogged] = useState(false);
 
+  const handleChange = (e) => {
+    setLogged(e.target.checked);
+  };
+
   return (
     <div>
       <h1>My App</h1>
-      <input type="checkbox" checked={logined} onChange={(e) => setLogged(e.target.checked)} /> 로그인 상태<br />
+      <input type="checkbox" checked={logined} onChange={handleChange} /> 로그인
+      상태
+      <br />
       <nav>
-        <NavLink to="/">Home</NavLink> | {" "}
-        <NavLink to="/about">About</NavLink>
+        <NavLink to="/">Home</NavLink> | <NavLink to="/about">About</NavLink>
       </nav>
       <Outlet context={{ logined }} />
     </div>
