08c234dfaada4ee1b78511947415ca7984486fac
the-eris-net <the.eris.net@gmail.com>
2025-09-14 14:40:23 +0900

    feat:add routing

--- 16p-init_react_router/src/App.jsx
+++ 16p-init_react_router/src/App.jsx
@@ -1,14 +1,7 @@
-import { Outlet, Link } from "react-router-dom";
-
 export default function App() {
   return (
     <div>
-      <h1>My App</h1>
-      <nav>
-        <Link to="/">Home</Link> |{" "}
-        <Link to="/about">About</Link>
-      </nav>
-      <Outlet />
+      Hello, World!
     </div>
   );
 }
--- 16p-init_react_router/src/main.jsx
+++ 16p-init_react_router/src/main.jsx
@@ -1,18 +1,13 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from "./App.jsx";
-import Home from "./Home.jsx";
-import About from "./About.jsx";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './App.jsx';
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />}>
-          <Route path="" element={<Home />} />
-          <Route path="about" element={<About />} />
-        </Route>
+        <Route path="/" element={<App />}></Route>
       </Routes>
     </BrowserRouter>
   </StrictMode>
