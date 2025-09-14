c1cce2e87523515c015f006465f3b20cc741951c
the-eris-net <the.eris.net@gmail.com>
2025-09-14 21:56:56 +0900

    feat:add useLocation

--- 38p-use_location/src/About.jsx
+++ 38p-use_location/src/About.jsx
@@ -18,21 +18,12 @@ export function Movie() {
   );
 }
 
-function getActiveStyle({isActive}) {
-  return {
-    backgroundColor: isActive ? "orange" : "inherit"
-  };
-}
-
 export default function About() {
   return (
     <div>
       <h2>About Page</h2>
       <nav>
-        <NavLink to="song">
-            노래</NavLink> | {" "}
-        <NavLink to="movie">
-            영화</NavLink> | {" "}
+        <NavLink to="song">노래</NavLink> | <NavLink to="movie">영화</NavLink> |{' '}
         <NavLink to="/">홈으로</NavLink>
       </nav>
       <Outlet />
---
+++ 38p-use_location/src/App1.jsx
@@ -0,0 +1,21 @@
+import { Outlet, NavLink, useLocation } from 'react-router-dom';
+import './App.css';
+
+export default function App() {
+  // const location = useLocation();
+  // console.log(location);
+  console.log(window.location);
+
+  return (
+    <div>
+      <h1>My App</h1>
+      {/* <p>현재 경로 hook : {location.pathname}</p> */}
+      <p>현재 경로 window : {window.location.pathname}</p>
+      <nav>
+        <NavLink to="/">Home</NavLink> | {" "}
+        <NavLink to="/about">About</NavLink>
+      </nav>
+      <Outlet />
+    </div>
+  );
+}
